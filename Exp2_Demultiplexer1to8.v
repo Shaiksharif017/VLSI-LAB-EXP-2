ee70730078365df3ced689ed6121386d3645b2d7
Shaiksharif017 <167750598+Shaiksharif017@users.noreply.github.com>
2024-05-13 13:56:51 +0530

    Update Exp2_Demultiplexer1to8.v

--- Exp2_Demultiplexer1to8.v
+++ Exp2_Demultiplexer1to8.v
@@ -1,9 +1,29 @@
-module demux_8(s,d,y);
-input [2:0]s;
-input d;
-output [7:0]y;
+module demultiplexer(d1,d2,d3,d4,d5,d6,d7,d8,i,s0,s1,s2);
 
+input i,s0,s1,s2;
 
+output d1,d2,d3,d4,d5,d6,d7,d8;
 
+wire w1,w2,w3;
+
+not g1(w1,s0);
+
+not g2(w2,s1);
+
+not g3(w3,s2);
+
+and g4(d1,w1,w2,w3,i);
+
+and g5(d2,w1,w2,s2,i);
+
+and g6(d3,w1,s1,w3,i);
+
+and g7(d4,w1,s1,s2,i);
+
+and g8(d5,s0,w2,w3,i); and g9(d6,s0,w2,s2,i);
+
+and g10(d7,s0,s1,w3,i);
+
+and g11(d8,s0,s1,s2,i);
 
 endmodule

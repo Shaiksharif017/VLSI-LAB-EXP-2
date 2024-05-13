8a4dc50df35982af5e9a3f9db017f22ea55fd392
Shaiksharif017 <167750598+Shaiksharif017@users.noreply.github.com>
2024-05-13 13:58:38 +0530

    Update Exp2_Multiplexer8to1.v

--- Exp2_Multiplexer8to1.v
+++ Exp2_Multiplexer8to1.v
@@ -1,8 +1,33 @@
-module mux_8(s0,s1,s2,d,y);
-input s0,s1,s2;
-input [7:0]d;
-output y;
+module multi(i,s,y);
 
+input[7:0]i;
 
+input[2:0]s;
+
+output reg y;
+
+always@(*)
+
+begin
+
+case({s[2],s[1],s[0]})
+
+3'b000:y=i[0];
+
+3'b001:y=i[1];
+
+3'b010:y=i[2];
+
+3'b011:y=i[3];
+
+3'b100:y=i[4];
+
+3'b101:y=i[5];
+
+3'b110:y=i[6];
+
+3'b111:y=i[7];
+
+endcase end
 
 endmodule

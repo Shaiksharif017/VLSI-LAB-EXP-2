1b8eb3987c81ed3f0254acb15e40bb7135181507
Shaiksharif017 <167750598+Shaiksharif017@users.noreply.github.com>
2024-05-13 13:56:12 +0530

    Update Exp2_Decoder3to8.v

--- Exp2_Decoder3to8.v
+++ Exp2_Decoder3to8.v
@@ -1,7 +1,29 @@
-module decoder_8(a,b,c,y);
-input a,b,c;
-output[7:0]y;
+module decoder(A,E,Y);
 
+input [1:0]A;
 
+input E;
+
+output [3:0]Y;
+
+assign Y[0]=~A[1]&~A[0]&E;
+
+assign Y[1]=~A[1]&A[0]&E;
+
+assign Y[2]=A[1]&~A[0]&E;
+
+assign Y[3]=A[1]&A[0]&E;
+
+endmodule
+
+module decoder(A,Y);
+
+input[2:0]A;
+
+output[7:0]Y;
+
+decoder_2_4 d1(A[1:0],~A[2],Y[3:0]);
+
+decoder_2_4 d2(A[1:0],~A[2],Y[7:4]);
 
 endmodule

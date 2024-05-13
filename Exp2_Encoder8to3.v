9b59afb4454c432ec881e3d207f6af665ba716cd
Shaiksharif017 <167750598+Shaiksharif017@users.noreply.github.com>
2024-05-13 13:57:27 +0530

    Update Exp2_Encoder8to3.v

--- Exp2_Encoder8to3.v
+++ Exp2_Encoder8to3.v
@@ -1,7 +1,13 @@
-module encoder_8(d,a,b,c);
+module encoder(d,a,b,c);
+
 input [7:0]d;
+
 output a,b,c;
 
+or (a,d[4],d[5],d[6],d[7]);
 
+or (b,d[2],d[3],d[6],d[7]);
+
+or (c,d[1],d[3],d[5],d[7]);
 
 endmodule
